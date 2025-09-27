4d3646cb68f734e9e5945ba8c1a04462528ca9f3
Admin <dance.with.me@bk.ru>
2025-09-27 14:10:57 +0700

    FirstBaseline

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.unit.sp
 import com.example.rowcoldemo.ui.theme.RowColDemoTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.LastBaseline
 
 
@@ -62,15 +63,16 @@ fun MainScreen(modifier: Modifier = Modifier) {
     Row {
         Text(
             text = "Large Text\n\nMore Text",
-            Modifier.alignBy(LastBaseline),
-            fontSize = 40.sp, fontWeight = FontWeight.Bold
+            Modifier.alignBy(FirstBaseline),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
         )
         Text(
             text = "Small Text",
-            Modifier.alignByBaseline(),
+            modifier = Modifier.paddingFrom(
+                alignmentLine = FirstBaseline, before = 80.dp, after = 0.dp),
             fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+            fontWeight = FontWeight.Bold )
     }
 }
 
